f53ee37da38de1930fbe94f01acf2c55b4955be7
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2021-11-25 12:25:11 +0100

    fix: enhance response validation for off-api.js

--- functions/src/off-api.js
+++ functions/src/off-api.js
@@ -8,10 +8,13 @@ module.exports = {
       try {
         request(baseUrl + productId, {json: true}, (err, res, body) => {
           if (err) reject(err);
-          if (typeof body === "object") {
+          // check whether a valid product json object is returned from off-api
+          // eslint-disable-next-line max-len
+          if (typeof body === "object" && "status" in body && body.status == 1) {
             resolve(body);
           } else {
-            reject(new Error("Server response is not JSON data"));
+            // eslint-disable-next-line max-len
+            reject(new Error("Server response is no valid product information"));
           }
         });
       } catch (e) {
