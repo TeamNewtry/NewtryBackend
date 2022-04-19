e22ffe19c5e52ba425606d7a539caa93eb5581bc
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2022-04-19 22:30:54 +0200

    add: 3 sec timeout for off-api

--- functions/src/off-api.js
+++ functions/src/off-api.js
@@ -1,23 +1,25 @@
 const functions = require("firebase-functions");
 const request = require("request");
 
+const offTimeout = 3000;
 const baseUrl = "https://de-de.openfoodfacts.org/api/v0/product/";
 
 module.exports = {
   getProduct(productId) {
     return new Promise((resolve, reject) => {
       try {
-        request(baseUrl + productId, {json: true}, (err, res, body) => {
-          if (err) reject(err);
-          // check whether a valid product json object is returned from off-api
-          // eslint-disable-next-line max-len
-          if (typeof body === "object" && "status" in body && body.status == 1) {
-            resolve(body);
-          } else {
-            // eslint-disable-next-line max-len
-            reject(new functions.https.HttpsError("unavailable", "Server response is no valid product information"));
-          }
-        });
+        request(baseUrl + productId, {json: true, timeout: offTimeout},
+            (err, res, body) => {
+              if (err) reject(err);
+              // check for valid product json object being returned from off-api
+              if (typeof body === "object" &&
+                "status" in body && body.status == 1) {
+                resolve(body);
+              } else {
+                // eslint-disable-next-line max-len
+                reject(new functions.https.HttpsError("unavailable", "Server response is no valid product information"));
+              }
+            });
       } catch (e) {
         reject(new functions.https.HttpsError("internal", e.toString()));
       }
