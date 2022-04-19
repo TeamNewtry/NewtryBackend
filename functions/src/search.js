c4863ad85a27c6a997c0a4c01aed1f6222756f83
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2022-04-20 00:27:15 +0200

    update: cleaned and migrated code to ECMA8

--- functions/src/search.js
+++ functions/src/search.js
@@ -6,46 +6,47 @@ const firestore = app.firestore();
 const productsCollection = firestore.collection("products");
 const MAX_SEARCH_RESULTS = 3;
 
-module.exports = {
-  getProductThatStartsWith(searchTerm) {
-    searchTerm = searchTerm.toString().toLowerCase();
-    const searchTermEnd = searchTerm.replace(/.$/,
-        (c) => String.fromCharCode(c.charCodeAt(0)+1));
+async function getProductThatStartsWith(searchTerm) {
+  searchTerm = searchTerm.toString().toLowerCase();
+  const searchTermEnd = searchTerm.replace(/.$/,
+      (c) => String.fromCharCode(c.charCodeAt(0)+1));
 
-    return new Promise((resolve, reject) => {
-      // first: try to fetch products with matching id
-      const productsIdPromise = productsCollection
-          .where("id", ">=", searchTerm)
-          .where("id", "<", searchTermEnd)
-          .limit(MAX_SEARCH_RESULTS)
+  try {
+    // first: try to fetch products with matching id
+    const idProducts = await productsCollection
+        .where("id", ">=", searchTerm)
+        .where("id", "<", searchTermEnd)
+        .limit(MAX_SEARCH_RESULTS)
+        .get();
+
+    // second: try to fill leftover slots with product name matches
+    if (idProducts.size < MAX_SEARCH_RESULTS) {
+      const nameProducts = await productsCollection
+          .where("product_name_lower", ">=", searchTerm)
+          .where("product_name_lower", "<", searchTermEnd)
+          .limit(MAX_SEARCH_RESULTS - idProducts.size)
           .get();
 
-      productsIdPromise.then((idProducts) => {
-        // second: try to fill leftover slots with product name matches
-        if (idProducts.size < MAX_SEARCH_RESULTS) {
-          const productsNamePromise = productsCollection
-              .where("product_name_lower", ">=", searchTerm)
-              .where("product_name_lower", "<", searchTermEnd)
-              .limit(MAX_SEARCH_RESULTS - idProducts.size)
-              .get();
+      if (idProducts.empty && nameProducts.empty) {
+        throw new functions.https.HttpsError("not-found",
+            "No matching product found!");
+      } else {
+        // concat the two results and map query snapshots to plain data
+        return idProducts.docs.concat(nameProducts.docs)
+            .map((snapshot) => snapshot.data());
+      }
+    } else { // or return all id-matching products (in case 5 have been found)
+      return idProducts.docs.map((snapshot) => snapshot.data());
+    }
+  } catch (err) {
+    if (!(err instanceof functions.https.HttpsError)) {
+      throw new functions.https.HttpsError("internal",
+          err.toString());
+    }
+    throw err;
+  }
+}
 
-          productsNamePromise.then((nameProducts) => {
-            if (idProducts.empty && nameProducts.empty) {
-              reject(new functions.https.HttpsError("not-found",
-                  "No matching product found!"));
-            } else {
-              // concat the two results and map query snapshots to plain data
-              resolve(idProducts.docs.concat(nameProducts.docs)
-                  .map((snapshot) => snapshot.data()));
-            }
-          });
-        // or return all id-matching products (in case 5 have been found)
-        } else {
-          // map query snapshots to plain data
-          resolve(idProducts.docs.map((snapshot) => snapshot.data()));
-        }
-      }).catch((err) => reject(new functions.https.HttpsError("internal",
-          err.toString())));
-    });
-  },
+module.exports = {
+  getProductThatStartsWith,
 };
